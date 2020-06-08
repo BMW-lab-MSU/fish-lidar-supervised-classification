309bed05ae8b833c9324bd7d05084e0a7d9ed469
Trevor Vannoy <trevorvannoy@montana.edu>
2020-06-08 13:46:32 -0600

    Merge branch 'cleanup' of github.com-tvannoy:tvannoy/afrl-project into cleanup

--- Yellowstone/normalize_surface_vect.m
+++ Yellowstone/normalize_surface_vect.m
@@ -1,12 +1,13 @@
-function surface_data = normalize_surface_vect(xpol,surface_index,depth)
+function surface_data = normalize_surface_vect(xpol,surface_index,depth, planeToSurf)
 
     N = length(surface_index);
-    surface_data = zeros(160,N);
+    surface_data = zeros(planeToSurf,N);
+    surfacePad = 10;
     
     for i = 1:N
-        start = surface_index(i) - 10;  % Start a little above the surface of the water
+        start = surface_index(i) - surfacePad;  % Start a little above the surface of the water
         stop = start + depth(i) - 1;
-        if stop - start < 159
+        if stop - start < (planeToSurf-1)
             surface_data(1:stop-start+1,i) = xpol(start:stop,i);
         else
             surface_data(:,i) = xpol(start:stop,i);
