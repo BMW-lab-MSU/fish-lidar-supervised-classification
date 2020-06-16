e3b1d5ffbc6cdc22033ec56a945cffa912cb8402
Trevor Vannoy <trevorvannoy@montana.edu>
2020-06-16 16:43:30 -0600

    add constants; fix linter suggestion
    MATLAB's linter suggested using 'first' or 'last' in find instead of finding the max

--- Yellowstone/create_positive_label_vect.m
+++ Yellowstone/create_positive_label_vect.m
@@ -1,5 +1,5 @@
 function [hits_vector] = create_positive_label_vect(avgWidthPerFish, school_sizes, distance, fish_distances)
-% Authors: Jackson Belford
+% Authors: Jackson Belford, Trevor Vannoy
 % Usage:    avgWidthPerFish is a value estimated to be the number of fish hits in a column 
 %                          on average. Generally around 4.25 for schooling
 %                          fish.
@@ -13,15 +13,23 @@ function [hits_vector] = create_positive_label_vect(avgWidthPerFish, school_size
 % Returns:  hits_vector, a vector of expanded fish labels. Labeling an area
 %                        fish in a school style identification.
 
+MAX_SCHOOL_WIDTH = 55;
+
+% The flight distances reported in the dataset and in the human labeled fish
+% hits do not match exactly, so we need a distance threshold what it means for
+% two distances to be the "same"
+DISTANCE_DIFFERENCE_THRESHOLD = 1.5;
+
 N = length(school_sizes);
 labelLength = zeros(N, 1);
 columnNumber = zeros(N, 1);
 
+% Set the length of the labels based upon the fish school size
 for idx = 1:N
 
     tempLength = ceil(school_sizes(idx) * avgWidthPerFish);
     
-    if tempLength >= 55
+    if tempLength >= MAX_SCHOOL_WIDTH
         tempLength = ceil(tempLength/4);                                   % To better agree with Kyles work, large values assume
     end                                                                    % a tight school instead of a massive school
     
@@ -30,10 +38,9 @@ for idx = 1:N
 end
 
 hits_vector = zeros(1, length(distance));
-
 for idy = 1:N
     
-    columnNumber(idy) = max(find(abs(distance-fish_distances(idy)) < 1.5)); % Produces a vector with ones centered around the column number
+    columnNumber(idy) = find(abs(distance-fish_distances(idy)) < DISTANCE_DIFFERENCE_THRESHOLD, 1, 'last'); % Produces a vector with ones centered around the column number
                                                                            % that corrisponds to the CSV.
     hits_vector((columnNumber(idy)-ceil(labelLength(idy)/2)):(columnNumber(idy) + ceil(labelLength(idy)/2))) = 1;
     
