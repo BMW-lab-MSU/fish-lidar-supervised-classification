f07b5518d7f762aa9a3dfdf4bf23e9960c45cc91
Trevor Vannoy <trevorvannoy@montana.edu>
2020-11-08 21:15:17 -0700

    update preprocessing and storage format
    we now compute the surface index ourselves, as well as saving all auxiliary data from the original h5 files

--- Yellowstone/preprocess.m
+++ Yellowstone/preprocess.m
@@ -1,16 +1,14 @@
-function preprocessed_data = preprocess(data, surface_pad, column_height)
-%% Data preprocessing
+function [xpol_processed, copol_processed] = preprocess(xpol_raw, copol_raw, surface_pad, column_height)
 %% Normalize surface index
 % The height of the water does not start at the same row in each LIDAR
 % shot. This could potentially cause problems with shift-variant
 % classifiers, so we want the height of the water to always start at 
 % the same row in every LIDAR shot. 
-surface_index = find_water_surface(data);
-preprocessed_data = normalize_surface_height(data, surface_index, surface_pad);
+surface_index = find_water_surface(copol_raw);
+xpol_processed = normalize_surface_height(xpol_raw, surface_index, surface_pad);
+copol_processed = normalize_surface_height(copol_raw, surface_index, surface_pad);
 
 %% Dimensionality reduction
-% Reduce column height to region of interest. The resulting image height
-% needs to be the same as what the classifier was trained against, which 
-% in this case is 60. 
-preprocessed_data = preprocessed_data(1:column_height,:);
+xpol_processed = xpol_processed(1:column_height,:);
+copol_processed = copol_processed(1:column_height,:);
 end
