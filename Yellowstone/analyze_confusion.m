b9a14613cb11cffe396a01f72635f436fb3be660
Trevor Vannoy <trevorvannoy@montana.edu>
2020-12-14 11:12:35 -0700

    fix f3-score formula

--- Yellowstone/analyze_confusion.m
+++ Yellowstone/analyze_confusion.m
@@ -18,6 +18,6 @@ TP = confmat(2,2);
 accuracy = (TP + TN)/(TP + TN + FP + FN);
 precision = (TP)/(TP + FP);
 recall = (TP)/(TP + FN);
-f3 = (1 + 3^2) * (precision * recall)/((1 + 3^2)*precision + recall);
+f3 = (1 + 3^2) * (precision * recall)/((3^2)*precision + recall);
 
 end
