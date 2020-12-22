4234a8fb57462ded6ba7512ef8a621964222f4a9
Trevor Vannoy <trevorvannoy@montana.edu>
2020-12-22 15:16:47 -0700

    test classifier and save results

--- GulfOfMexico/test_classifier.m
+++ GulfOfMexico/test_classifier.m
@@ -1,55 +1,114 @@
-%% Select testing variables
+%%
+addpath('../common')
 
-% Pick the classifier
-%boxdir = 'C:\Users\bradl\Box Sync\AFRL_Data\Data\GulfOfMexico\GoM Processed Classifier/';
-boxdir = '/home/trevor/research/AFRL/Box/Data/GulfOfMexico/GoM Processed Classifier/';
-modeldir = [boxdir '/Classifiers_Trained_On_09-24_Data/'];
-modelfiles = dir([modeldir, 'trainedModel*']);
+%% Setup data paths
+box_dir = '/mnt/data/trevor/research/afrl/box/Data/GulfOfMexico/processed data';
 
-% Load the classifier
-for idx = 1:length(modelfiles)
-    modelstruct = load([modeldir modelfiles(idx).name]);
-    modelcell = struct2cell(modelstruct);
-    models{idx} = modelcell{2};
-end
+datapaths = {
+    [box_dir filesep 'processed_data_09-24'],
+    [box_dir filesep 'processed_data_09-25'],
+    [box_dir filesep 'processed_data_09-26'],
+    [box_dir filesep 'processed_data_09-29'],
+    [box_dir filesep 'processed_data_09-30'],
+    [box_dir filesep 'processed_data_10-01'],
+    [box_dir filesep 'processed_data_10-02'],
+    [box_dir filesep 'processed_data_10-03'],
+    [box_dir filesep 'processed_data_10-04'],
+    [box_dir filesep 'processed_data_10-05'],
+    [box_dir filesep 'processed_data_10-07'],
+};
 
-% Pick the days to analyze
-path1 = [boxdir 'CLASSIFICATION_DATA_09-25.mat'];
-path2 = [boxdir 'CLASSIFICATION_DATA_09-26.mat'];
-path3 = [boxdir 'CLASSIFICATION_DATA_09-29.mat'];
-path4 = [boxdir 'CLASSIFICATION_DATA_09-30.mat'];
-path5 = [boxdir 'CLASSIFICATION_DATA_10-01.mat'];
-path6 = [boxdir 'CLASSIFICATION_DATA_10-02.mat'];
-path7 = [boxdir 'CLASSIFICATION_DATA_10-03.mat'];
-path8 = [boxdir 'CLASSIFICATION_DATA_10-04.mat'];
-path9 = [boxdir 'CLASSIFICATION_DATA_10-05.mat'];
-path10 = [boxdir 'CLASSIFICATION_DATA_10-07.mat'];
+%% Load the classifier
+disp('loading classifier')
+trained_model_dir = '.';
+load([trained_model_dir filesep 'rusBoost']);
 
-% Put it all together!
-datapath_list = {path1, path2, path3, path4, path5, path6, path7, path8, path9, path10};
+%% Region of interest parameters
+WINDOW_SIZE = 1000;
+OVERLAP = 0.1;
+TRUE_LABEL_THRESHOLD = 1;
+PREDICTED_LABEL_THRESHOLD = 10;
 
-%% Define variables
-num_days = length(datapath_list);
-conf_all = zeros(2,2,num_days);
-accuracy = zeros(1,num_days);
-precision = zeros(1,num_days);
-recall = zeros(1,num_days);
+%% Run the classifier on each day
+results = containers.Map();
 
+for datapath_idx = 1:numel(datapaths)
+    datapath = datapaths{datapath_idx};
+    date = datapath(end-4:end);
 
-%% Run the classifier on each day!
-for model_idx = 1:length(models)
-    for idx = 1:num_days
-        datapath = datapath_list(idx);
-        datapath = string(datapath);
-        datapath_char = convertStringsToChars(datapath);
-        disp(['Running classifier on ...' datapath_char(end-29:end)]);
-        [conf_all(:,:,idx),predicted_labels{idx}, labels{idx}] = predict_labels(datapath, models{model_idx});
-        [accuracy(idx), precision(idx), recall(idx)] = analyze_confusion(conf_all(:,:,idx));
-    end
+    disp(['running on ' date '...']);
+
+    disp('loading data...')
+    load(datapath, 'xpol_processed', 'labels');
     
-    % Display & Save Results
-    conf_all
-    acc_prec_rec = [accuracy; precision; recall]
-    save([modeldir 'results_' modelfiles(model_idx).name],'conf_all','accuracy','precision','recall','predicted_labels','labels');
+    % turn labels into binary labels: nothing or any of {fish, school}
+    labels = any(labels(1:2,:));
+
+    disp('classifying...')
+    shot.Labels = labels;
+    shot.PredictedLabels = logical(rusBoost.predictFcn(xpol_processed.'));
+    shot.Confusion = confusionmat(shot.Labels, shot.PredictedLabels);
+    [~, precision, recall, f3] = analyze_confusion(shot.Confusion);
+    shot.Precision = precision;
+    shot.Recall = recall;
+    shot.F3 = f3;
+
+    roi.Labels = groupLabels(labels, TRUE_LABEL_THRESHOLD, WINDOW_SIZE, OVERLAP);
+    roi.PredictedLabels = groupLabels(shot.PredictedLabels, ...
+        PREDICTED_LABEL_THRESHOLD, WINDOW_SIZE, OVERLAP);
+    roi.Confusion = confusionmat(roi.Labels, roi.PredictedLabels);
+    [~, precision, recall, f3] = analyze_confusion(roi.Confusion);
+    roi.Precision = precision;
+    roi.Recall = recall;
+    roi.F3 = f3;
+
+    results(date) = struct('Shot', shot, 'Roi', roi);
 end
 
+%% Create summary of results from all days
+shot = struct();
+roi = struct();
+roi.F3 = 0;
+roi.Recall = 0;
+roi.Precision = 0;
+roi.Confusion = zeros(2);
+shot.F3 = 0;
+shot.Recall = 0;
+shot.Precision = 0;
+shot.Confusion = zeros(2);
+
+disp('computing average results');
+for date = results.keys
+    shot.Precision = shot.Precision + results(date{:}).Shot.Precision;
+    shot.Recall = shot.Recall + results(date{:}).Shot.Recall;
+    shot.F3 = shot.F3 + results(date{:}).Shot.F3;
+    shot.Confusion = shot.Confusion + results(date{:}).Shot.Confusion;
+
+    roi.Precision = roi.Precision + results(date{:}).Roi.Precision;
+    roi.Recall = roi.Recall + results(date{:}).Roi.Recall;
+    roi.F3 = roi.F3 + results(date{:}).Roi.F3;
+    roi.Confusion = roi.Confusion + results(date{:}).Roi.Confusion;
+end
+
+% average results
+shot.Precision = shot.Precision / double(results.Count);
+shot.Recall = shot.Recall / double(results.Count);
+shot.F3 = shot.F3 / double(results.Count);
+roi.Precision = roi.Precision / double(results.Count);
+roi.Recall = roi.Recall / double(results.Count);
+roi.F3 = roi.F3 / double(results.Count);
+
+results('average') = struct('Shot', shot, 'Roi', roi);
+
+save('results', 'results', '-v7.3');
+
+%% Display results
+for date = results.keys
+    disp([date{:} ' results'])
+    disp('shot')
+    disp(results(date{:}).Shot)
+    disp(results(date{:}).Shot.Confusion)
+    disp('roi')
+    disp(results(date{:}).Roi)
+    disp(results(date{:}).Roi.Confusion)
+end
