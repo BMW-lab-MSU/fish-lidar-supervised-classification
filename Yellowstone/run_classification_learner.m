79382772a96271e241748234c99c75656a0728fd
Trevor Vannoy <trevorvannoy@montana.edu>
2021-01-22 13:52:17 -0700

    remove unused files

--- Yellowstone/run_classification_learner.m
+++
@@ -1,8 +0,0 @@
-box_dir = '/mnt/data/trevor/research/afrl/box/Data/Yellowstone';
-training_data_filename = 'training_data_2016_undersampled75';
-
-load([box_dir filesep training_data_filename])
-
-classification_data = [data.xpol_processed; data.labels].';
-
-classificationLearner
