d57497d886591bbfdbdb900043f8d6cc7ef3cffa
Trevor Vannoy <trevorvannoy@montana.edu>
2021-04-26 16:02:48 -0600

    initial bayesopt test

--- Yellowstone/train_crossval.m
+++ Yellowstone/train_crossval.m
@@ -2,7 +2,7 @@
 addpath('../common');
 clear
 
-box_dir = '/mnt/data/trevor/research/AFRL/Box/Data/Yellowstone';
+box_dir = '/mnt/data/trevor/research/afrl/AFRL_Data/Data/Yellowstone';
 
 pool = parpool();
 statset('UseParallel', true);
@@ -11,20 +11,59 @@ statset('UseParallel', true);
 load([box_dir filesep 'training' filesep 'training_data.mat']);
 
 %%
-trained_models = cell(1, crossval_partition.NumTestSets);
-crossval_confusion = zeros(2, 2, 1);
 
+sigma = optimizableVariable('sigma',[1e-5,1e5],'Transform','log');
+box = optimizableVariable('box',[1e-5,1e5],'Transform','log');
+standardize = optimizableVariable('standardize', [0 1], 'Type', 'integer');
+fncost = optimizableVariable('fncost', [1 20], 'Type', 'integer');
 
-tic
-for i = 1:crossval_partition.NumTestSets
-    validation_set_data = training_data(:, test(crossval_partition, i));
-    validation_set_labels = training_labels(test(crossval_partition, i));
-    training_set_data = training_data(:, training(crossval_partition, i));
-    training_set_labels = training_labels(training(crossval_partition, i));
+minfn = @(z)svmobjfun(z, crossval_partition, training_data, training_labels);
 
-    trained_models{i} = fitcsvm(training_set_data.', training_set_labels, 'Cost', [0 1; 10 0]);
+results = bayesopt(minfn, [sigma, box, standardize, fncost], 'IsObjectiveDeterministic', true, 'AcquisitionFunctionName', 'expected-improvement-plus', 'UseParallel', true);
+    % tic
+    % % for i = 1:crossval_partition.NumTestSets
+    % for i = 2
+    %     validation_set_data = training_data(:, test(crossval_partition, i));
+    %     validation_set_labels = training_labels(test(crossval_partition, i));
+    %     training_set_data = training_data(:, training(crossval_partition, i));
+    %     training_set_labels = training_labels(training(crossval_partition, i));
 
-    pred_labels = predict(trained_models{i}, validation_set_data.');
-    crossval_confusion(:, :, i) = confusionmat(validation_set_labels, pred_labels);
+    %     trained_models{i} = fitcsvm(training_set_data.', training_set_labels, 'Cost', [0 1; 10 0]);
+
+    %     pred_labels = predict(trained_models{i}, validation_set_data.');
+
+    %     crossval_confusion(:, :, i) = confusionmat(validation_set_labels, pred_labels);
+
+    %     losses(i) = loss(trained_models{i}, validation_set_data.', validation_set_labels)
+    % end
+    % toc
+
+    % kfold_loss = mean(losses)
+
+function [objective, constraints, userdata] = svmobjfun(z, crossval_partition, training_data, training_labels)
+    trained_models = cell(1, crossval_partition.NumTestSets);
+    crossval_confusion = zeros(2, 2, 1);
+    losses = zeros(1, 1);
+
+    % for i = 1:crossval_partition.NumTestSets
+    for i = 1
+        validation_set_data = training_data(:, test(crossval_partition, i));
+        validation_set_labels = training_labels(test(crossval_partition, i));
+        training_set_data = training_data(:, training(crossval_partition, i));
+        training_set_labels = training_labels(training(crossval_partition, i));
+
+        trained_models{i} = fitcsvm(training_set_data.', training_set_labels, 'Cost', [0 1; z.fncost 0], 'BoxConstraint', z.box, 'KernelScale', z.sigma, 'Standardize', logical(z.standardize));
+
+        pred_labels = predict(trained_models{i}, validation_set_data.');
+
+        crossval_confusion(:, :, i) = confusionmat(validation_set_labels, pred_labels);
+
+        losses(i) = loss(trained_models{i}, validation_set_data.', validation_set_labels)
+    end
+    
+    objective = mean(losses)
+
+    userdata.trained_models = trained_models;
+    userdata.confusion = crossval_confusion;
+    userdata.losses = losses;
 end
-toc
