60fb828594611cbb77571971aa9b993dfbda4cec
Trevor Vannoy <trevorvannoy@montana.edu>
2021-04-27 14:44:44 -0600

    testing undersampling/oversampling

--- Yellowstone/train_crossval.m
+++ Yellowstone/train_crossval.m
@@ -4,14 +4,13 @@ clear
 
 box_dir = '/mnt/data/trevor/research/afrl/AFRL_Data/Data/Yellowstone';
 
-pool = parpool();
-statset('UseParallel', true);
+% pool = parpool();
+% statset('UseParallel', true);
 
 %% Load data
 load([box_dir filesep 'training' filesep 'training_data.mat']);
 
 %%
-
 sigma = optimizableVariable('sigma',[1e-5,1e5],'Transform','log');
 box = optimizableVariable('box',[1e-5,1e5],'Transform','log');
 standardize = optimizableVariable('standardize', [0 1], 'Type', 'integer');
@@ -19,15 +18,15 @@ fncost = optimizableVariable('fncost', [1 20], 'Type', 'integer');
 
 minfn = @(z)svmobjfun(z, crossval_partition, training_data, training_labels);
 
-results = bayesopt(minfn, [sigma, box, standardize, fncost], 'IsObjectiveDeterministic', true, 'AcquisitionFunctionName', 'expected-improvement-plus', 'UseParallel', false, 'MaxObjectiveEvaluations', 1);
+results = bayesopt(minfn, [sigma, box, standardize, fncost], 'IsObjectiveDeterministic', true, 'AcquisitionFunctionName', 'expected-improvement-plus', 'UseParallel', false, 'MaxObjectiveEvaluations', 3);
 
 
 
 %% SVM objective function for Bayesian optimization
 function [objective, constraints, userdata] = svmobjfun(z, crossval_partition, training_data, training_labels)
     trained_models = cell(1, crossval_partition.NumTestSets);
-    crossval_confusion = zeros(2, 2, 1);
-    losses = zeros(1, 1);
+    crossval_confusion = zeros(2, 2, crossval_partition.NumTestSets);
+    losses = zeros(1, crossval_partition.NumTestSets);
 
     for i = 1:crossval_partition.NumTestSets
         validation_set_data = training_data(:, test(crossval_partition, i));
@@ -35,13 +34,30 @@ function [objective, constraints, userdata] = svmobjfun(z, crossval_partition, t
         training_set_data = training_data(:, training(crossval_partition, i));
         training_set_labels = training_labels(training(crossval_partition, i));
 
-        trained_models{i} = fitcsvm(training_set_data.', training_set_labels, 'Cost', [0 1; z.fncost 0], 'BoxConstraint', z.box, 'KernelScale', z.sigma, 'Standardize', logical(z.standardize));
+        sum(training_set_labels == 0)
+        sum(training_set_labels == 1)
 
-        pred_labels = predict(trained_models{i}, validation_set_data.');
+        % Undersample the majority class
+        idx_remove = random_undersample(training_set_labels, 0, 'UndersamplingRatio', 0.75);
+        training_set_data(:, idx_remove) = [];
+        training_set_labels(idx_remove) = [];
+
+        % Oversample the minority class
+        [synthetic_fish, synthetic_fish_labels] = ADASYN(training_set_data', ...
+            training_set_labels, 0.5, [], [], false);
+        training_set_data = [training_set_data, synthetic_fish'];
+        training_set_labels = [training_set_labels, synthetic_fish_labels'];
+
+        sum(training_set_labels == 0)
+        sum(training_set_labels == 1)
+
+        trained_models{i} = fitcsvm(training_set_data', training_set_labels, 'Cost', [0 1; z.fncost 0], 'BoxConstraint', z.box, 'KernelScale', z.sigma, 'Standardize', logical(z.standardize));
+
+        pred_labels = predict(trained_models{i}, validation_set_data');
 
         crossval_confusion(:, :, i) = confusionmat(validation_set_labels, pred_labels);
 
-        losses(i) = loss(trained_models{i}, validation_set_data.', validation_set_labels);
+        losses(i) = loss(trained_models{i}, validation_set_data', validation_set_labels);
     end
     
     objective = mean(losses);
