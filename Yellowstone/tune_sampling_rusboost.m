10556f85a630035bb103a644c36650a76c1f3865
Trevor Vannoy <trevorvannoy@montana.edu>
2021-05-04 11:37:14 -0600

    split sampling ratio tuning into multiple files

---
+++ Yellowstone/tune_sampling_rusboost.m
@@ -0,0 +1,21 @@
+%% Setup
+addpath('../common');
+%clear
+
+box_dir = '/mnt/data/trevor/research/afrl/AFRL_Data/Data/Yellowstone';
+
+%pool = parpool();
+%statset('UseParallel', true);
+
+%% Load data
+load([box_dir filesep 'training' filesep 'training_data.mat']);
+
+%% Tune sampling ratios
+tune_sampling_base(@rusboost, training_data, training_labels, ...
+    crossval_partition);
+
+%% Model fitting function
+function model = rusboost(data, labels, ~)
+    t = templateTree('Reproducible',true);
+    model = fitcensemble(data, labels, 'Method', 'RUSBoost', 'Learners', t);
+end
---
+++ Yellowstone/tune_sampling_tree.m
@@ -0,0 +1,19 @@
+%% Setup
+addpath('../common');
+%clear
+
+box_dir = '/mnt/data/trevor/research/afrl/AFRL_Data/Data/Yellowstone';
+
+%pool = parpool();
+%statset('UseParallel', true);
+
+%% Load data
+load([box_dir filesep 'training' filesep 'training_data.mat']);
+
+%% Tune sampling ratios
+tune_sampling_base(@tree, training_data, training_labels, crossval_partition);
+
+%% Model fitting function
+function model = tree(data, labels, ~)
+    model = fitctree(data, labels); 
+end
