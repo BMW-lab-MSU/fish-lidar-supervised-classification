0d12f8325832a5e5c55b22160be6215006293e06
Trevor Vannoy <trevorvannoy@montana.edu>
2021-05-04 21:41:44 -0600

    compact models; transpose training data and labels

--- Yellowstone/tune_sampling_nb.m
+++ Yellowstone/tune_sampling_nb.m
@@ -9,11 +9,13 @@ box_dir = '/mnt/data/trevor/research/afrl/AFRL_Data/Data/Yellowstone';
 
 %% Load data
 load([box_dir filesep 'training' filesep 'training_data.mat']);
+training_data = training_data';
+training_labels = training_labels';
 
 %% Tune sampling ratios
 tune_sampling_base(@nb, training_data, training_labels, crossval_partition);
 
 %% Model fitting function
 function model = nb(data, labels, ~)
-    model = fitcnb(data, labels); 
+    model = compact(fitcnb(data, labels)); 
 end
