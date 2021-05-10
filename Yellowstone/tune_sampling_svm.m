d66a0ccd0c4e92de903eec95e4d2204cd6cbed53
Trevor Vannoy <trevorvannoy@montana.edu>
2021-05-10 08:50:44 -0600

    use fitcsvm instead of fitclinear
    fitcsvm performed better in this, although it was significantly slower

--- Yellowstone/tune_sampling_svm.m
+++ Yellowstone/tune_sampling_svm.m
@@ -3,9 +3,11 @@ addpath('../common');
 %clear
 rng(0, 'twister');
 
-box_dir = '/mnt/data/trevor/research/afrl/AFRL_Data/Data/Yellowstone';
+box_dir = '../../data/fish-lidar/Data/Yellowstone';
 
-%pool = parpool();
+if isempty(gcp('nocreate'))
+    pool = parpool();
+end
 %statset('UseParallel', true);
 
 %% Load data
@@ -14,9 +16,10 @@ training_data = training_data';
 training_labels = training_labels';
 
 %% Tune sampling ratios
-tune_sampling_base(@svm, training_data, training_labels, crossval_partition);
+tune_sampling_base(@svm, training_data, training_labels, crossval_partition, ...
+    'Progress', true, 'UseParallel', true, 'NumThreads', 2);
 
 %% Model fitting function
 function model = svm(data, labels, ~)
-    model = compact(fitclinear(data, labels)); 
+    model = compact(fitcsvm(data, labels)); 
 end
