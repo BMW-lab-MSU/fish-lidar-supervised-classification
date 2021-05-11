b224241fed8973a09d189b6ef5c9d0771192f16f
Trevor Vannoy <trevorvannoy@montana.edu>
2021-05-10 20:44:26 -0600

    split data based upon ROIs

--- GulfOfMexico/create_rand_training_data.m
+++ GulfOfMexico/create_rand_training_data.m
@@ -5,9 +5,9 @@ clear
 % Set random number generator properties for reproducibility
 rng(0, 'twister');
 
-box_dir = '/mnt/data/trevor/research/afrl/AFRL_Data/Data/GulfOfMexico';
+box_dir = '/mnt/data/trevor/research/AFRL/Box/Data/GulfOfMexico';
 
-input_data_files = {...
+data_files = {...
     'processed_data_09-24.mat'...
     'processed_data_09-25.mat'...
     'processed_data_09-26.mat'...
@@ -20,41 +20,68 @@ input_data_files = {...
     'processed_data_10-05.mat'...
     'processed_data_10-06.mat'...
     'processed_data_10-07.mat'};
+day = {'09-24', '09-25', '09-26', '09-29', '09-30', '10-01', '10-02', ...
+    '10-03', '10-04', '10-05', '10-06', '10-07'};
+
 
 %% Load data
-data = [];
-labels = [];
-for filename = input_data_files
-    tmp = load([box_dir filesep 'processed data' filesep filename{:}], 'xpol_processed', 'labels');
-    data = [data, single(tmp.xpol_processed)];
-    labels = logical([labels, logical(tmp.labels)]);
+data = struct('data', cell(numel(data_files), 1), ...
+    'labels', cell(numel(data_files), 1), 'year', cell(numel(data_files), 1));
+
+for i = 1:numel(data_files)
+    tmp = load([box_dir filesep 'processed data' filesep data_files{i}], 'xpol_processed', 'labels');
+    data(i).data = single(tmp.xpol_processed)';
+    data(i).labels = any(tmp.labels)';
+    data(i).day = day{i};
 end
 
-labels = any(labels(1:4,:));
+%% Partition data into regions of interest
+WINDOW_SIZE = 1000;
+OVERLAP = 0;
+
+roi = struct('data', cell(numel(data_files), 1), ...
+    'labels', cell(numel(data_files), 1), ...
+    'year', cell(numel(data_files), 1), ...
+    'indicator', cell(numel(data_files), 1));
+
+for i = 1:numel(data_files)
+    roi(i).labels = create_regions(data(i).labels, WINDOW_SIZE, OVERLAP);
+    roi(i).data = create_regions(data(i).data, WINDOW_SIZE, OVERLAP);
+    roi(i).indicator = cellfun(@(c) any(c), roi(i).labels);
+    roi(i).day = day{i};
+end
+
+roi_data = vertcat(roi(:).data);
+roi_labels = vertcat(roi(:).labels);
+roi_indicator = vertcat(roi(:).indicator);
 
 %% Partiion into training and test sets
 TEST_PCT = 0.2;
 
-holdout_partition = cvpartition(labels, 'Holdout', TEST_PCT, 'Stratify', true);
+holdout_partition = cvpartition(roi_indicator, 'Holdout', TEST_PCT, 'Stratify', true);
 
-training_labels = labels(training(holdout_partition));
-testing_labels = labels(test(holdout_partition));
-training_data = data(:, training(holdout_partition));
-testing_data = data(:, test(holdout_partition));
-
+training_labels = roi_labels(training(holdout_partition));
+testing_labels = roi_labels(test(holdout_partition));
+training_data = roi_data(training(holdout_partition), :);
+testing_data = roi_data(test(holdout_partition), :);
+training_roi_indicator = roi_indicator(training(holdout_partition));
+testing_roi_indicator = roi_indicator(test(holdout_partition));
 
 %% Partition the data for k-fold cross validation
-N_FOLDS = 5;
+N_FOLDS = 3;
 
-crossval_partition = cvpartition(training_labels, 'KFold', N_FOLDS, 'Stratify', true);
+crossval_partition = cvpartition(training_roi_indicator, 'KFold', N_FOLDS, 'Stratify', true);
 
 
 %% Save training and testing data
 mkdir(box_dir, 'testing');
-save([box_dir filesep 'testing' filesep 'testing_data_all_labels.mat'], ...
-    'testing_data', 'testing_labels', 'holdout_partition', '-v7.3');
+save([box_dir filesep 'testing' filesep 'roi_testing_data.mat'], ...
+    'testing_data', 'testing_labels', 'testing_roi_indicator', ...
+    'holdout_partition', '-v7.3');
 
 mkdir(box_dir, 'training');
-save([box_dir filesep 'training' filesep 'training_data_all_labels.mat'], ...
-    'training_data', 'training_labels', 'crossval_partition', ...
-    'holdout_partition', '-v7.3');
+save([box_dir filesep 'training' filesep 'roi_training_data.mat'], ...
+    'training_data', 'training_labels', 'training_roi_indicator',...
+    'crossval_partition', 'holdout_partition', '-v7.3');
+
+save([box_dir filesep 'full_roi_data.mat'], 'roi', '-v7.3');
--- Yellowstone/create_rand_training_data.m
+++ Yellowstone/create_rand_training_data.m
@@ -5,43 +5,69 @@ clear
 % Set random number generator properties for reproducibility
 rng(0, 'twister');
 
-box_dir = '/mnt/data/trevor/research/afrl/AFRL_Data/Data/Yellowstone';
-input_data_files = {'processed_data_2015', 'processed_data_2016'};
+box_dir = '/mnt/data/trevor/research/AFRL/Box/Data/Yellowstone';
+data_files = {'processed_data_2015', 'processed_data_2016'};
+years = {'2015', '2016'};
 
 
 %% Load data
-data = [];
-labels = [];
-for filename = input_data_files
-    tmp = load([box_dir filesep filename{:}], 'xpol_processed', 'labels');
-    data = [data, single(tmp.xpol_processed)];
-    labels = logical([labels, logical(tmp.labels)]);
+data = struct('data', cell(numel(data_files), 1), ...
+    'labels', cell(numel(data_files), 1), 'year', cell(numel(data_files), 1));
+
+for i = 1:numel(data_files)
+    tmp = load([box_dir filesep data_files{i}], 'xpol_processed', 'labels');
+    data(i).data = single(tmp.xpol_processed)';
+    data(i).labels = logical(tmp.labels)';
+    data(i).year = years{i};
 end
 
+%% Partition data into regions of interest
+WINDOW_SIZE = 1000;
+OVERLAP = 0;
+
+roi = struct('data', cell(numel(data_files), 1), ...
+    'labels', cell(numel(data_files), 1), ...
+    'year', cell(numel(data_files), 1), ...
+    'indicator', cell(numel(data_files), 1));
+
+for i = 1:numel(data_files)
+    roi(i).labels = create_regions(data(i).labels, WINDOW_SIZE, OVERLAP);
+    roi(i).data = create_regions(data(i).data, WINDOW_SIZE, OVERLAP);
+    roi(i).indicator = cellfun(@(c) any(c), roi(i).labels);
+    roi(i).year = years{i};
+end
+
+roi_data = vertcat(roi(:).data);
+roi_labels = vertcat(roi(:).labels);
+roi_indicator = vertcat(roi(:).indicator);
 
 %% Partiion into training and test sets
 TEST_PCT = 0.2;
 
-holdout_partition = cvpartition(labels, 'Holdout', TEST_PCT, 'Stratify', true);
+holdout_partition = cvpartition(roi_indicator, 'Holdout', TEST_PCT, 'Stratify', true);
 
-training_labels = labels(training(holdout_partition));
-testing_labels = labels(test(holdout_partition));
-training_data = data(:, training(holdout_partition));
-testing_data = data(:, test(holdout_partition));
-
+training_labels = roi_labels(training(holdout_partition));
+testing_labels = roi_labels(test(holdout_partition));
+training_data = roi_data(training(holdout_partition), :);
+testing_data = roi_data(test(holdout_partition), :);
+training_roi_indicator = roi_indicator(training(holdout_partition));
+testing_roi_indicator = roi_indicator(test(holdout_partition));
 
 %% Partition the data for k-fold cross validation
-N_FOLDS = 5;
+N_FOLDS = 3;
 
-crossval_partition = cvpartition(training_labels, 'KFold', N_FOLDS, 'Stratify', true);
+crossval_partition = cvpartition(training_roi_indicator, 'KFold', N_FOLDS, 'Stratify', true);
 
 
 %% Save training and testing data
 mkdir(box_dir, 'testing');
-save([box_dir filesep 'testing' filesep 'testing_data.mat'], ...
-    'testing_data', 'testing_labels', 'holdout_partition');
+save([box_dir filesep 'testing' filesep 'roi_testing_data.mat'], ...
+    'testing_data', 'testing_labels', 'testing_roi_indicator', ...
+    'holdout_partition');
 
 mkdir(box_dir, 'training');
-save([box_dir filesep 'training' filesep 'training_data.mat'], ...
-    'training_data', 'training_labels', 'crossval_partition', ...
-    'holdout_partition');
+save([box_dir filesep 'training' filesep 'roi_training_data.mat'], ...
+    'training_data', 'training_labels', 'training_roi_indicator',...
+    'crossval_partition', 'holdout_partition');
+
+save([box_dir filesep 'full_roi_data.mat'], 'roi');
