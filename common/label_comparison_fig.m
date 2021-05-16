2d925e153529404407d5468cb18b16de984a790f
Trevor Vannoy <trevorvannoy@montana.edu>
2021-05-16 12:23:01 -0600

    update with new colors; remove distance

--- common/label_comparison_fig.m
+++ common/label_comparison_fig.m
@@ -1,4 +1,4 @@
-function fig = label_comparison_fig(labels, predicted_labels, data, depth_increment, distance)
+function fig = label_comparison_fig(labels, predicted_labels, data, depth_increment)
 
 LABEL_HEIGHT = 3;
 
@@ -7,6 +7,8 @@ labels = labels(:)';
 predicted_labels = predicted_labels(:)';
 
 depth = [-ones(1, LABEL_HEIGHT * 2), depth_increment * (0:height(data) - 1)];
+
+distance = 1:length(data);
 
 
 %% lidar data image
@@ -19,7 +21,7 @@ ax1.YTick = ax1.YTick(2:end);
 
 ax1.FontSize = 12;
 ax1.YLabel.String = 'Depth [m]';
-ax1.XLabel.String = 'Distance [m]';
+ax1.XLabel.String = 'Distance [shots]';
 
 % remove axis box so the axis forms an "L" instead of a box; this makes the
 % figure look better because the labels image cuts off the top part of the axis
@@ -39,7 +41,7 @@ ax2.YTick = [];
 % https://www.mathworks.com/matlabcentral/answers/194554-how-can-i-use-and-display-two-different-colormaps-on-the-same-figure
 linkaxes([ax1, ax2]);
 colormap(ax1, flipud(colormap(brewermap(10000,'*PuBu'))));
-colormap(ax2, [255, 255, 255; 163, 193, 102; 103, 163, 193]/255);
+colormap(ax2, [255, 255, 255; 220, 49, 0; 133, 194, 226]/255);
 
 
 %% legend
@@ -47,7 +49,7 @@ colormap(ax2, [255, 255, 255; 163, 193, 102; 103, 163, 193]/255);
 % add "fake" lines so we can have a legend for the label blocks
 % https://stackoverflow.com/questions/12902709/how-to-add-legend-in-imagesc-plot-in-matlab
 L = line(ones(2),ones(2), 'LineWidth',2);
-set(L,{'color'},mat2cell([163, 193, 102; 103, 163, 193]/255,ones(1,2),3));
+set(L,{'color'},mat2cell([220, 49, 0; 133, 194, 226]/255,ones(1,2),3));
 
 legendobj = legend({'human labels', 'predicted labels'}, 'Location', 'southeast', 'EdgeColor', [1 1 1], 'FontSize', 12);
 
