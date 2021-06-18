27daa96d0af9788569273e698208838637b9d36e
Trevor Vannoy <trevorvannoy@montana.edu>
2021-06-18 13:33:52 -0600

    initial changes to record runtimes

--- Yellowstone/tune_sampling_first_day_tree.m
+++ Yellowstone/tune_sampling_first_day_tree.m
@@ -12,9 +12,24 @@ box_dir = '/mnt/data/trevor/research/AFRL/Box/Data/Yellowstone';
 load([box_dir filesep 'training' filesep 'first_day_roi_training_data.mat']);
 
 %% Tune sampling ratios
+tic
 result = tune_sampling_roi_base(@tree, training_roi_data, ...
     training_roi_labels, training_roi_indicator, crossval_partition, ...
     'Progress', true);
+runtime = toc;
+
+% save runtimes
+tab = table(runtime, 'VariableNames', "sampling_tuning_runtime", 'RowNames', "tree");
+if exist(['runtimes' filesep 'sampling_tuning_runtimes'])
+    runtimes_tab = load(['runtimes' filesep 'sampling_tuning_runtimes']);
+    runtimes_tab = [runtimes_tab; tab];
+else
+    runtimes_tabl = tab;
+    mkdir('runtimes');
+end
+runtimes_tab = load(['runtimes' filesep 'sampling_tuning_runtimes']);
+runtimes_tab = [runtimes_tab; tab];
+save('sampling_tuning_runtimes', 'runtimes_tab')
 
 save([box_dir filesep 'training' filesep 'sampling_tuning_first_day_roi_tree.mat'], 'result')
 
