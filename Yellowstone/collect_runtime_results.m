84af2bc038e6d1edf9eb8169c0f999c30bf9d898
Trevor Vannoy <trevorvannoy@montana.edu>
2021-12-02 17:54:09 -0700

    Update runtime tables
    - Remove the first iteration; it's always slow because MATLAB is
      interpreted.
    - Round numbers for display purposes

--- Yellowstone/collect_runtime_results.m
+++ Yellowstone/collect_runtime_results.m
@@ -8,18 +8,23 @@ runtime_struct = struct();
 for model = model_names
     load([box_dir filesep model{:} '_streaming_simulation'])
     
-    total = prediction_runtime + preprocessing_runtime;
+    % The first function call is always slow because MATLAB is
+    % interpretted: https://stackoverflow.com/a/19500758
+    % So I'm removing the first iteration
+    prediction = prediction_runtime(2:end);
+    preprocessing = preprocessing_runtime(2:end);
+    total = prediction + preprocessing;
     
-    model_runtimes.mean_prediction = mean_prediction_runtime;
-    model_runtimes.mean_preprocessing = mean_preprocessing_runtime;
+    model_runtimes.mean_prediction = mean(prediction);
+    model_runtimes.mean_preprocessing = mean(preprocessing);
     model_runtimes.mean_total = mean(total);
 
-    model_runtimes.max_prediction = max_prediction_runtime;
-    model_runtimes.max_preprocessing = max_preprocessing_runtime;
+    model_runtimes.max_prediction = max(prediction);
+    model_runtimes.max_preprocessing = max(preprocessing);
     model_runtimes.max_total = max(total);
     
-    model_runtimes.std_prediction = std_prediction_runtime;
-    model_runtimes.std_preprocessing = std_preprocessing_runtime;
+    model_runtimes.std_prediction = std(prediction);
+    model_runtimes.std_preprocessing = std(preprocessing);
     model_runtimes.std_total = std(total);
     
     runtime_struct.(model{:}) = model_runtimes;
@@ -29,6 +34,9 @@ runtime_table = struct2table(struct2array(runtime_struct));
 
 % convert seconds to milliseconds
 runtime_table.Variables = runtime_table.Variables * 1000;
+
+% round to 2 decimal places
+runtime_table.Variables = round(runtime_table.Variables, 2);
 
 runtime_table = [model_names' runtime_table];
 runtime_table.Properties.VariableNames{1} = 'model';
